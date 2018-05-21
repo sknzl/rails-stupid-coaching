57f1f4ddfcc7e01736147fd65cb1791bb74dd3fd
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-21 08:55:15 +0100

    stupid coaching finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'questions#ask'
+  # get 'questions/ask'
+  get 'ask', to: 'questions#ask', as: :ask
+  # get 'questions/answer'
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/system/questions_test.rb
@@ -0,0 +1,19 @@
+# test/system/questions_test.rb
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "h3", text: "Ask your coach anything"
+  end
+
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Submit"
+    # take_screenshot
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
