e296a69da4ce6b8129a60a5a5dd3f4d75b006afa
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-21 14:40:38 +0100

    test included

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -11,8 +11,10 @@ class QuestionsTest < ApplicationSystemTestCase
   test "saying Hello yields a grumpy response from the coach" do
     visit ask_url
     fill_in "question", with: "Hello"
+    # binding.pry
+    #  do.click
     click_on "Submit"
-    # take_screenshot
+    take_screenshot
 
     assert_text "I don't care, get dressed and go to work!"
   end
